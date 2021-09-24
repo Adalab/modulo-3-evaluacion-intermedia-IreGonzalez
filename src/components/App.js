f29c709b65fed4d36d74a9215750890b1709c3c5
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-24 08:45:16 +0200

    add files

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+
+import '../styles/App.scss';
+// Importamos el servicio del local storage
+import ls from '../services/local-Storage';
+// importamos useEffect además de useState
+import { useEffect, useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
