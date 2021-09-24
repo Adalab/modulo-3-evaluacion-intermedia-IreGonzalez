1d165a09b1a44d73834641d4da5128254277b2c6
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-24 10:00:36 +0200

    paint clubs

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
 import '../styles/App.scss';
-// Importamos el servicio del local storage
-import ls from '../services/local-storage';
+import initialData from '../data/clubs.json'
 // importamos useEffect además de useState
 import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState(initialData);
+  const weekDays = [data.id].openOnWeekdays ? 'Si' : 'No';
+  const weekEnd = [data.id].openOnWeekend ? 'Si' : 'No';
+
+  const htmlClubList = data.map((data, index) => (
+    <li className="club" key={index} id={index}>
+      <h3>{data.name}</h3>
+      <button>X</button>
+      <p>Abierto entre semana : {weekDays}</p>
+      <p>Abierto el fin de semana : {weekEnd}</p>
+    </li>
+  )
+  );
+
   return (
     <div className="App">
       <header>
@@ -19,32 +33,8 @@ function App() {
         </form>
       </header>
       <main>
-        <section>
-          <div>
-            <h3>#0: Book club</h3>
-            <button>X</button>
-            <p>Abierto entre semana : Sí</p>
-            <p>Abierto el fin de semana : Sí</p>
-          </div>
-          <div>
-            <h3>#1: Chess club</h3>
-            <button>X</button>
-            <p>Abierto entre semana : Sí</p>
-            <p>Abierto el fin de semana : Sí</p>
-          </div>
-          <div>
-            <h3>#2: Escape room club</h3>
-            <button>X</button>
-            <p>Abierto entre semana : Sí</p>
-            <p>Abierto el fin de semana : Sí</p>
-          </div>
-          <div>
-            <h3>#3: Debate club</h3>
-            <button>X</button>
-            <p>Abierto entre semana : Sí</p>
-            <p>Abierto el fin de semana : Sí</p>
-          </div>
-        </section>
+        <ul>{htmlClubList}
+        </ul>
         <section>
           <h2>Añadir un nuevo club</h2>
           <form>
